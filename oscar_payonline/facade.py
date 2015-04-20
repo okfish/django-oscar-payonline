374b4ff5b3cfe220a279f7fd7cf2c2049b24087c
Fish <okfish@yandex.ru>
2015-04-20 22:38:48 +0300

    defrost_basket shortcut method added to the facade

--- oscar_payonline/facade.py
+++ oscar_payonline/facade.py
@@ -1,10 +1,16 @@
 import random
 
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
+from django.db.models import get_model
+
 from oscar.apps.payment.exceptions import UnableToTakePayment, InvalidGatewayRequestError
 
+
 from payonline.models import PaymentData
+
+Basket = get_model('basket', 'Basket')
 
 def merchant_reference(merchant_id, basket_id):
     # Ideas stolen from Oscar's Datacash facade
@@ -14,6 +20,11 @@ def merchant_reference(merchant_id, basket_id):
     # instance.  Hence we can get a clash of merchant references.
     rand = "%04.f" % (random.random() * 10000)
     return u'%s-%s-%s' % (merchant_id, basket_id, rand)
+
+def defrost_basket(basket_id):
+        basket = get_object_or_404(Basket, id=basket_id,
+                                   status=Basket.FROZEN)
+        basket.thaw()    
 
 class Facade(object):
     """
