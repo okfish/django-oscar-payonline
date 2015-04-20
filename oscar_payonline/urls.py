db774f8211e051ed58439ca7f6c2620948660a2e
Fish <okfish@yandex.ru>
2015-04-20 23:26:38 +0300

    payonline-place-order url updated

--- oscar_payonline/urls.py
+++ oscar_payonline/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns(
     url(r'^$', RedirectView.as_view(), name='payonline-pay'),
     url(r'^callback/$', CallbackView.as_view(), name='payonline-callback'),
     url(r'^fail/$', FailView.as_view(), name='payonline-fail'),
-    url(r'^success/$', SuccessView.as_view(), name='payonline-success'),
+    url(r'^success/$', SuccessView.as_view(preview=True), name='payonline-success'),
+    url(r'^place-order/(?P<basket_id>\d+)/$', SuccessView.as_view(),
+        name='payonline-place-order'),
 )
