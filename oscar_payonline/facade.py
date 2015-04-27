f1b2de483db357a93177083d596696c3d4417aa0
Fish <okfish@yandex.ru>
2015-04-27 19:55:24 +0300

    Main working codebase includes all functions required for checkout. No refunds implemented yet. Transaction can be confirmed only locally (PaymentData should exist), remote calls for txn check not implemented. So, set up callbacks properly in the Payonline panel.

--- oscar_payonline/facade.py
+++ oscar_payonline/facade.py
@@ -3,14 +3,18 @@ import random
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
-from django.db.models import get_model
 
+from oscar.core.loading import get_class, get_model
 from oscar.apps.payment.exceptions import UnableToTakePayment, InvalidGatewayRequestError
 
 
 from payonline.models import PaymentData
 
+from .exceptions import PayOnlineError
+
 Basket = get_model('basket', 'Basket')
+Applicator = get_class('offer.utils', 'Applicator')
+Selector = get_class('partner.strategy', 'Selector')
 
 def merchant_reference(merchant_id, basket_id):
     # Ideas stolen from Oscar's Datacash facade
@@ -22,9 +26,59 @@ def merchant_reference(merchant_id, basket_id):
     return u'%s-%s-%s' % (merchant_id, basket_id, rand)
 
 def defrost_basket(basket_id):
-        basket = get_object_or_404(Basket, id=basket_id,
-                                   status=Basket.FROZEN)
-        basket.thaw()    
+    basket = get_object_or_404(Basket, id=basket_id,
+                               status=Basket.FROZEN)
+    basket.thaw()    
+
+def load_frozen_basket(request, basket_id):
+    # Ideas stolen from Oscar's PayPal facade
+    
+    # Lookup the frozen basket that this txn corresponds to
+    try:
+        basket = Basket.objects.get(id=basket_id, status=Basket.FROZEN)
+    except Basket.DoesNotExist:
+        return None
+
+    # Assign strategy to basket instance
+    if Selector:
+        basket.strategy = Selector().strategy(request, request.user)
+
+    # Re-apply any offers
+    Applicator().apply(basket, request.user, request)
+
+    return basket
+
+def fetch_transaction_details(ref):
+    txn = None
+    try:
+        txn = PaymentData.objects.get(order_id=ref)
+    except PaymentData.DoesNotExist:
+        msg = "Error for %s: PaymentData does not exists" % ref
+        raise PayOnlineError(msg)
+    return txn
+
+def confirm_transaction(ref, amount, currency):
+    """
+    Confirms that transaction corrensponding to given ref-string
+    exists in our database (the callback has been triggered)
+    and TODO: Payonline should return the same info. 
+    Otherwise, two conditions can be reached:
+     - we have txn recorded but no txn found via PayOnline API 
+       so we mark txn as suspicious
+     - we have no txn records but Payonline tells that txn is ok 
+       (e.g. txn was ok but no callbacks triggered or data saved)
+       so we should save it again or mark that txn as requiring OP confirmation   
+    """
+    try:
+        txn = fetch_transaction_details(ref)
+    except PayOnlineError:
+        raise
+    if txn.amount != amount or txn.currency != currency:
+        msg = ("Error for %s: amount %s or currency %s "
+              "does not match requested (%s %s)" % (ref, amount, currency, 
+                                                    txn.amount, txn.currency ))
+        raise PayOnlineError(msg)
+    return txn
 
 class Facade(object):
     """
